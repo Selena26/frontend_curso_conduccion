db5de9f0e7efe6c30dd2c75e11078424999d70c9
Selena26 <serivas@espe.edu.ec>
2023-08-31 23:30:16 -0500

    desarrollo de pruebas unitarias y revision de servicios

--- src/app/services/usuario.service.spec.ts
+++ src/app/services/usuario.service.spec.ts
@@ -1,16 +1,118 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { UsuarioService } from './usuario.service';
 
 describe('UsuarioService', () => {
   let service: UsuarioService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UsuarioService]
+    });
+
     service = TestBed.inject(UsuarioService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Deberia guardar usuario', () => {
+    const usuario = { /* usuario data */ };
+
+    service.saveUsuario(usuario).then(response => {
+      expect(response).toBeTruthy();
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/usuario');
+    expect(req.request.method).toBe('POST');
+    req.flush({});
+  });
+
+  it('Deberia obtener usuarios', () => {
+    service.getUsers().then(users => {
+      expect(users).toBeTruthy();
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/usuarios');
+    expect(req.request.method).toBe('GET');
+    req.flush([]);
+  });
+
+  it('Deberia obtener un usuario', () => {
+    service.getUsers1().then(users => {
+      expect(users).toBeTruthy();
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/usuarios1');
+    expect(req.request.method).toBe('GET');
+    req.flush([]);
+  });
+
+  it('Deberia obtener usuarios por ID', () => {
+    const userId = 1;
+
+    service.getUserById(userId).then(user => {
+      expect(user).toBeTruthy();
+    });
+
+    const req = httpMock.expectOne(`http://localhost:3000/usuario/getById/${userId}`);
+    expect(req.request.method).toBe('GET');
+    req.flush({});
+  });
+
+  it('Deberia actualizar usuarios', () => {
+    const userId = 1;
+    const usuario = { /* updated usuario data */ };
+
+    service.updateUsuario(userId, usuario).then(response => {
+      expect(response).toBeTruthy();
+    });
+
+    const req = httpMock.expectOne(`http://localhost:3000/usuario/update/${userId}`);
+    expect(req.request.method).toBe('PUT');
+    req.flush({});
+  });
+
+  it('Deberia eliminar un usuario', () => {
+    const userId = 1;
+
+    service.deleteUsuario(userId).then(response => {
+      expect(response).toBeTruthy();
+    });
+
+    const req = httpMock.expectOne(`http://localhost:3000/usuario/delete/${userId}`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush({});
+  });
+
+  it('Deberia chequear si un correo existe', () => {
+    const email = 'test@example.com';
+
+    service.checkEmailExists(email).subscribe(response => {
+      expect(response.emailExists).toBe(true); // You can adjust this based on your test data
+    });
+
+    const req = httpMock.expectOne(`http://localhost:3000/usuario/checkEmail/${email}`);
+    expect(req.request.method).toBe('GET');
+    req.flush({ emailExists: true }); // You can adjust this based on your test data
+  });
+
+  it('Deberia chequear si la cedula existe', () => {
+    const cedula = '1234567890';
+    service.checkCedulaExists(cedula).subscribe(response => {
+      expect(response.cedulaExists).toBe(true); // You can adjust this based on your test data
+    });
+
+    const req = httpMock.expectOne(`http://localhost:3000/usuario/checkCedula/${cedula}`);
+    expect(req.request.method).toBe('GET');
+    req.flush({ cedulaExists: true }); // You can adjust this based on your test data
   });
 });
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,21 +1,16 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
   url = 'http://localhost:3000/';
-
-
-
   constructor(public http:HttpClient, public https:HttpClient) { }
-
-
-
   saveUsuario(usuario:any){
     return new Promise(resolve => {
-      this.https.post(this.url+'usuario',usuario).subscribe({
+      this.http.post(this.url+'usuario',usuario).subscribe({
         next: (data) => {
           resolve(data);
         },
@@ -25,10 +20,9 @@ export class UsuarioService {
       });
     });
   }
-
   getUsers(){
     return new Promise(resolve => {
-      this.https.get(this.url+'usuarios').subscribe({
+      this.http.get(this.url+'usuarios').subscribe({
         next: (data) => {
           resolve(data);
         },
@@ -38,7 +32,6 @@ export class UsuarioService {
       });
     });
   }
-
   getUsers1(){
     return new Promise(resolve => {
       this.http.get(this.url+'usuarios1').subscribe({
@@ -51,9 +44,6 @@ export class UsuarioService {
       });
     });
   }
-
-
-
   getUserById(id: number){
     return new Promise(resolve => {
       this.http.get(this.url + 'usuario/getById/' + id).subscribe({
@@ -66,8 +56,6 @@ export class UsuarioService {
       });
     });
   }
-
-
   updateUsuario(id: number, usuario: any) {
     return new Promise((resolve, reject) => {
       this.http.put(this.url + 'usuario/update/' + id, usuario).subscribe({
@@ -81,7 +69,6 @@ export class UsuarioService {
       });
     });
   }
-
   deleteUsuario(id: number) {
     return new Promise((resolve, reject) => {
       this.http.delete(this.url + 'usuario/delete/' + id).subscribe({
@@ -95,7 +82,10 @@ export class UsuarioService {
       });
     });
   }
-
-
-
+  checkEmailExists(email: string): Observable<{ emailExists: boolean }> {
+    return this.http.get<{ emailExists: boolean }>(`${this.url}usuario/checkEmail/${email}`);
+  }
+  checkCedulaExists(cedula: string): Observable<{ cedulaExists: boolean }> {
+    return this.http.get<{ cedulaExists: boolean }>(`${this.url}usuario/checkCedula/${cedula}`);
+  }
 }
